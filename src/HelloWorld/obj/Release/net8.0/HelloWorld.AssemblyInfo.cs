bf9f947fb075e4ae1bc2345efc5d0a94e19ecf6c
Bryce Thompson <9068303+thompsonbryce@users.noreply.github.com>
2024-09-02 07:50:34 +0800

    WIP

--- src/HelloWorld/obj/Release/net8.0/HelloWorld.AssemblyInfo.cs
+++ src/HelloWorld/obj/Release/net8.0/HelloWorld.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+488ceeccfe7b41769c137e987c2aca44f1d87a1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
