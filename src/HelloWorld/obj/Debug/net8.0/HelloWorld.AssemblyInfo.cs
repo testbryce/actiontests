122f968362deb73c2946a683fe6f89d9eaa38976
Bryce Thompson <9068303+thompsonbryce@users.noreply.github.com>
2024-09-05 08:46:26 +0800

    printjob

--- src/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ src/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+488ceeccfe7b41769c137e987c2aca44f1d87a1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f2197a31f4493bd6cafc49f59a58417f907a6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
